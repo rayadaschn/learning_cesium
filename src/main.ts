5dd30832c6cb15c9222277d9114da2e9c0386418
rayadaschn <115447518+rayadaschn@users.noreply.github.com>
2024-05-30 00:18:20 +0800

    feat: 更新配置

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 
-const sysBaseUrl = import.meta.env.BASE_URL;
-const mode = import.meta.env.MODE;
+// 下面是为了查看一下配置是否正常~
+const sysBaseUrl = import.meta.env.BASE_URL; // vite.config.ts 中的 base 配置
+const mode = import.meta.env.MODE; // 当前模式
 const sourceCesiumBaseUrl = import.meta.env.VITE_CESIUM_BASE_URL;
 
-// 在不同的 base 路径下（vite.config.ts 中的 base 配置
-// 尤其是开发模式用的是拷贝来的 CesiumJS 库文件，最好拼接上 base 路径
 // 生产模式使用 CDN 则不用拼接 base 路径
 const cesiumBaseUrl =
   mode === "development"
